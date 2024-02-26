ffd378004c803b324245e877fa79efe0b4dc835e
Eduard Ivankovich <eduard.ivankovich@yandex.ru>
2024-02-26 01:44:13 +0100

    фикс верстки фильмов

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,8 +1,8 @@
 export const SHORTMOVIE = 40;
 
 export const DESKTOP_LAYOUT_WIDTH = 1280;
-export const TABLET_LAYOUT_WIDTH = 800;
-export const MOBILE_LAYOUT_WIDTH = 600;
+export const TABLET_LAYOUT_WIDTH = 1217;
+export const MOBILE_LAYOUT_WIDTH = 731;
 
 export const DESKTOP_LAYOUT_SUM = 12;
 export const TABLET_LAYOUT_SUM = 8;
